91a014bdfa5082e0bc1ae4107cf9c60298ef2929
Adam Carroll <Oceloteater@users.noreply.github.com>
2022-07-05 21:20:16 +0200

    Adding auth module for adding users to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const connect = require('../config.js');
 console.log('connect', connect);
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(connect)],
+  imports: [AuthModule, TasksModule, TypeOrmModule.forRoot(connect)],
   controllers: [AppController],
   providers: [AppService],
 })
